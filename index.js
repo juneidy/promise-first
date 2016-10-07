686f984b59b383b7f3de55ac1b0cfc85fb814b8e
juneidy <juneidy@amristar.com>
2016-10-07 13:29:00 +0800

    Added check for count. Check if all promise have been fulfilled without reaching count.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 if (!Promise.first) {
 	Promise.first = (ps, count = 1) => (
-		new Promise(res => {
+		new Promise((res, rej) => {
+			// Invalid count
+			count < 1 && rej()
+
+			let total = ps.length
 			const vals = []
 
+			// Reject when all finished
+			function decrement () {
+				!--total && rej()
+			}
+
+			// Resolve when count reached
 			function countDown (v) {
 				if (count--) {
 					vals.push(v)
 					!count && res(vals)
 				}
+				decrement()
 			}
 
-			ps.forEach(p => (
-				Promise.resolve(p).then(countDown)
-			))
+			ps.forEach(p => Promise.resolve(p).then(countDown, decrement))
 		})
 	)
 }
